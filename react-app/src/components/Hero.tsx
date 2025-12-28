38bfcde0de44dbddf77c3db9ec02c8aefa2e6f7f
larvenejafemcoder <tomkancaston@gmail.com>
2025-12-28 13:39:30 +0700

    new

--- react-app/src/components/Hero.tsx
+++ react-app/src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CoffeeLoader from './CoffeeLoader'
 
 export default function Hero(){
   return (
@@ -7,7 +8,11 @@ export default function Hero(){
         <div className="text"><h1>A Pleasure<br/>In<br/>Life.</h1></div>
         <button>More</button>
       </div>
-      <div className="right-content" aria-hidden="true"></div>
+
+        <div className="right-content">
+          <CoffeeLoader />
+        </div>
+
     </section>
   )
 }
